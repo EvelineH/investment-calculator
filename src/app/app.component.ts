b556f159f2dc98f4976d6ece182155039f5d6a6b
Eveline Huiser <eveline.huiser@capgemini.com>
2024-10-22 11:35:04 +0200

    migrated to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import type { InvestmentInput } from './investment-input.model';
 import { InvestmentsResultsComponent } from './investments-results/investments-results.component';
 
 @Component({
@@ -11,37 +10,5 @@ import { InvestmentsResultsComponent } from './investments-results/investments-r
   imports: [HeaderComponent, UserInputComponent, InvestmentsResultsComponent],
 })
 export class AppComponent {
-  resultsData?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
 
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const annualData = [];
-    const { initialInvestment, annualInvestment, expectedReturn, duration } =
-      data;
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData = annualData;
-  }
 }
